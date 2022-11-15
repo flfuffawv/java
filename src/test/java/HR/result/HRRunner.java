95f60a633a3af0ce1315e3d4dee0f8e0a89df25d
flfuffawv <115691682+flfuffawv@users.noreply.github.com>
2022-11-15 16:16:17 +0800

    Update HRRunner.java

--- src/test/java/HR/result/HRRunner.java
+++ src/test/java/HR/result/HRRunner.java
@@ -14,8 +14,8 @@ import org.springframework.test.context.ContextConfiguration;
         monochrome = true,
         glue = {"HR.stepDefinitions"},
 
-        features = "src/test/java/HR/Feature/登陆.feature",plugin = {"html:target/HR-reports/HRlogin.html","json:target/HR-reports/HRlogin.json","junit:target/HR-reports/HRlogin.xml","pretty"}
-
+       // features = "src/test/java/HR/Feature/登陆.feature",plugin = {"html:target/HR-reports/HRlogin.html","json:target/HR-reports/HRlogin.json","junit:target/HR-reports/HRlogin.xml","pretty"}
+       features = "src/test/java/HR/Feature/基础资料公司.feature",plugin = {"html:target/HR-reports/HRCompany.html","json:target/HR-reports/HRCompany.json","junit:target/HR-reports/HRCompany.xml","pretty"}
         // tags = "@委托协议输入－送样 "
 
 
